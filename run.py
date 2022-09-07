495877d157d6424609bd71c4d83a75a40b17315f
Shanbashir1 <shanbashir1@hotmail.com>
2022-09-07 18:45:19 +0000

    Add user request loop to repeat for data untill data provided is valid

--- run.py
+++ run.py
@@ -10,24 +10,25 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love_sandwiches')
-
+SHEET = GSPREAD_CLIENT.open('my_love_sandwiches')
 
 def get_sales_data():
     """
-    Get sales figures input from the user.
+    Get sales figures input from the user
     """
     while True:
-        print("Please enter sales data from the last market.")
-        print("Data should be six numbers, separated by commas.")
-        print("Example: 10,20,30,40,50,60\n")
+        print('Please enter sales figures from the last market.')
+        print('Data should be six numbers, seperated by commas')
+        print('Example: 10,20,30,40,50,60\n')
 
-        data_str = input("Enter your data here: ")
+        data_str = input('Enter your data here:')
+    
 
-        sales_data = data_str.split(",")
+        sales_data = data_str.split(',')
+
 
         if validate_data(sales_data):
-            print("Data is valid!")
+            print('Data is valid!')
             break
 
     return sales_data
@@ -41,15 +42,14 @@ def validate_data(values):
     """
     try:
         [int(value) for value in values]
-        if len(values) != 6:
+        if len(values) !=6:
             raise ValueError(
-                f"Exactly 6 values required, you provided {len(values)}"
+                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")
-        return False
+          print(f"Invalid data: {e}, please try again.\n")
+          return False
 
     return True
-
 
 data = get_sales_data()
